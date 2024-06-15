4a236a32d30a8acf22bfef62d53f1ef3d779b069
ravindar-10 <work.ravindar@gmail.com>
2024-06-15 18:16:27 +0530

    Revert "mod4 assignment done"
    This reverts commit 5bcd8d8d682f871052b57a14f347127366793960.
    
    	deleted:    rust_assignment/employee/Cargo.toml
    	deleted:    rust_assignment/employee/src/main.rs
    	deleted:    rust_assignment/mod4-assignment/Cargo.toml
    	deleted:    rust_assignment/mod4-assignment/README.md
    	deleted:    rust_assignment/mod4-assignment/src/main.rs
    	deleted:    rust_assignment/mod4-assignment/src/matching.rs
    	deleted:    rust_assignment/mod4-assignment/src/slice.rs
    	deleted:    rust_assignment/mod4-assignment/src/structs.rs
    	deleted:    rust_assignment/mod4-assignment/src/variables.rs

--- rust_assignment/mod4-assignment/src/variables.rs
+++
@@ -1,9 +0,0 @@
-pub fn variables_example() {
-    // declare a variable with value 1
-    let mut x = 1;
-    println!("x = {}", x);
-
-    // change the value of variable x
-    x = 2;
-    println!("x = {}", x);
-}
